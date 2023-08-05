1a371674156dbdaf370bce4152dcf2a3e5644b5d
chadmuro <chadmuro@gmail.com>
2023-08-05 23:13:40 +0900

    feat: Set cache on medium posts fetch

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,14 @@
 import type { Article } from '$lib/types';
 
-export async function load({ fetch }) {
+export async function load({ fetch, setHeaders }) {
 	const res = await fetch(
 		'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40chadmuro'
 	);
 	const posts = await res.json();
+
+	setHeaders({
+		'Cache-Control': 'max-age=3600'
+	});
 
 	return { posts: posts.items as Article[] };
 }
